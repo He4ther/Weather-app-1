5b89979655d1860f728b8b90e1e5ee19873b2470
heather duffy <heatherlouiseduffy@gmail.com>
2022-06-02 21:10:37 +0100

    Created function dispalyForecast

--- src/app.js
+++ src/app.js
@@ -13,7 +13,34 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
 
+    let forecastHTML = `<div class="row>`;
+    let days = ["Thu", "Fri", "Sat"];
+   days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+         <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="https://cdn3.iconfinder.com/data/icons/tango-icon-library/48/weather-clear-512.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span
+                  ><span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+     
+  `;
+  });
+    
+ forecastHTML = forecastHTML + `</div>`;
+ forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -23,6 +50,7 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
    
+
     celsiusTemperature = response.data.main.temp;
    
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
@@ -60,6 +88,8 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
