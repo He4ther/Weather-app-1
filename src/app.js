9ecbecc1048646368361cb33305e43b421052518
heather duffy <heatherlouiseduffy@gmail.com>
2022-06-03 20:25:20 +0100

    Created forecast variable

--- src/app.js
+++ src/app.js
@@ -13,30 +13,40 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row>`;
-    let days = ["Thu", "Fri", "Sat"];
-   days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
     forecastHTML =
       forecastHTML +
       `
          <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
                 <img
                   src="https://cdn3.iconfinder.com/data/icons/tango-icon-library/48/weather-clear-512.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> 18° </span
-                  ><span class="weather-forecast-temperature-min"> 12° </span>
+                  <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span
+                  ><span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
                 </div>
               </div>
      
   `;
+        }
   });
     
  forecastHTML = forecastHTML + `</div>`;
